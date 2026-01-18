42d93ec70fbfa496c3d18b04ad6dc3407a912d95
Brandy <Bengc102@gmail.com>
2026-01-18 17:10:25 +0100

    version changes 2

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: {
-    domains: ["localhost", "res.cloudinary.com"],
     unoptimized: true,
     remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "5000",
-        pathname: "/**",
-      },
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
